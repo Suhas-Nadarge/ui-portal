1672f5e84ce09283ea7bfd307739534b2c2171ff
Suhas Nadarge <suhas.nadarge@mnx.com>
2021-12-19 19:22:04 +0000

    commit-3

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-
-//Install express server
 const express = require('express');
 const path = require('path');
-const http = require('http')
+
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(_dirname,'dist/ui-portal')));
+app.use(express.static(__dirname + '/dist/ui-portal'));
 
-app.get('*', function(req,res) {
+app.get('/*', function(req,res) {
     
-res.sendFile(path.join(_dirname,'/dist/ui-portal/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ui-portal/index.html'));
 });
 
-const port = process.env.PORT || 3000
-app.set('port',port)
-const server = http.createServer(app)
 // Start the app by listening on the default Heroku port
-server.listen(port, ()=> console.log('running'));
+app.listen(process.env.PORT || 8080);
